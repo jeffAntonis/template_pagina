f1f40c753c3db61c134f0ef78d5108b1e4b0a6bc
Jefferson Antonis Santos <jasilva3@stefanini.com>
2019-02-28 17:17:17 -0300

    Melhorias diversas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection } from 'react-mdl';
 import Main from './components/main';
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,7 @@ const App = (props) => {
     return (
         <div className="demo-big-content">
         <Layout >
-            <Header className="header-color" title="DoubleMJ" scroll>
+            <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">DoubleMJ</Link>} scroll>
                 <Navigation>
                     <Link to="/resume">Resume</Link>
                     <Link to="/projects">Projects</Link>
@@ -16,18 +16,22 @@ const App = (props) => {
                     <Link to="/about">About</Link>
                 </Navigation>
             </Header>
-            <Drawer title="Title">
+            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">DoubleMJ</Link>}>
                 <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contacts">Contacts</Link>
+                    <Link to="/about">About</Link>
                 </Navigation>
             </Drawer>
             <Content>
                 <div className="page-content" />
                 <Main />
             </Content>
+            <Footer size="mega">
+                <FooterSection type="bottom" logo="© 2018 Copyright: DoubleMJ">
+                </FooterSection>
+            </Footer>
         </Layout>
     </div>
     );
